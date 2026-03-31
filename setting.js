85f333b0bb9caca5fd698a5011191690d87bb64f
fabiomarinheiro75-dev <fabiomarinheiro75@gmail.com>
2026-03-30 22:01:20 -0300

    Update bot configuration settings

--- setting.js
+++ setting.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   BOT_NAME: 'ZARAKI_BOT', // ur bot name
-  OWNER_NAME: '@kurosaki', //your name
+  OWNER_NAME: '@kurosaki_san', //your name
   OWNER_NUMBER: '11918648657', // add your phone number
-  SESSION_ID: '001', // add your session ID
+  SESSION_ID: 'kurosaki', // add your session ID
   MODE: "public", // public or private
-  TIMEZONE: 'Brasil/natal' // ur timezone
+  TIMEZONE: 'Brasil/brasilia' // ur timezone
 };
